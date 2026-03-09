fb563f0edd0ee2a5d0551f6a96d80f8b22c5c1c3
tumampoctroyg <tumampoctroyg@gmail.com>
2026-03-10 03:51:19 +0800

    Create Act#2.py

---
+++ Act#2.py
@@ -0,0 +1,15 @@
+FullName = ("Troy Gian A. Tumampoc")
+print("FullName:", FullName)
+print(type(FullName))
+
+Age = 19
+print("Age:", Age)
+print(type(Age))
+
+Height = 1.65
+print("Height in school:", Height)
+print(type(Height))
+
+in_school = True
+print("Currently in school?", in_school)
+print(type(in_school))
